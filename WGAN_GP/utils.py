2dc995c5567ed03b7a534221d6a80cc2260f9353
nishanth7803 <80695412+nishanth7803@users.noreply.github.com>
2023-09-18 16:46:21 +0530

    Add files via upload

---
+++ WGAN_GP/utils.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+
+def gradient_penalty(critic, real, fake, device="cpu"):
+    batch_size, c, h, w = real.shape
+    epsilon = torch.rand((batch_size, 1, 1, 1)).repeat(1, c, h, w).to(device)
+    interpolated_images = real * epsilon + fake * (1 - epsilon)
+
+    mixed_scores = critic(interpolated_images)
+
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
+
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+def show_tensor_images(image_tensor, num_images=25, size=(3, 64, 64)):
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
