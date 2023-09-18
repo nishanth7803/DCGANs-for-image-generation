b512db2db2d45a0a58fa37d070fe7739fed980ef
nishanth7803 <80695412+nishanth7803@users.noreply.github.com>
2023-09-18 16:39:59 +0530

    Create utils.py

---
+++ DCGAN/utils.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
+
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+def show_tensor_images(image_tensor, num_images=25, size=(3, 64, 64)):
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
